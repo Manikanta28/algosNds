406cfbec8c87fc6bdbbc82b039573ee477911418
ManiKanta <maniakantak@zeta.tech>
2020-04-04 11:55:34 +0530

    reverse method in single linked list

--- ds/LinkedLists/singleLinkedLists.ts
+++ ds/LinkedLists/singleLinkedLists.ts
@@ -80,6 +80,25 @@ class SingleLinkedList {
       this.length--;
     }
   }
+  public reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
 }
 
 var myLinkedList = new SingleLinkedList(1);
